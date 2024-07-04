08f5762a6178116a5be4c277506ea897e60d07aa
koleso8 <dev.koleso@gmail.com>
2024-07-04 12:51:04 +0300

    й

---
+++ js/task-1.js
@@ -0,0 +1,11 @@
+const categories = document.querySelectorAll('.item')
+
+const calcCategories = categories.length;
+const res = `Number of categories : ${calcCategories}`
+
+console.log(res)
+
+
+
+
+
